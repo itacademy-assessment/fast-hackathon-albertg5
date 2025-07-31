e4635f735d515e309d2e62e4e3830c6c446038c2
Alex Gesti - Mr.Luigi001 <40181553+alexgesti@users.noreply.github.com>
2025-07-31 12:41:14 +0200

    header-allicando

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -1,7 +1,18 @@
 const Header = () => {
 	return (
-		<header className="flex bg-gradient-to-r from-purple-600 via-white to-white p-4 shadow mb-6">
-			<h1 className="text-2xl font-bold"> IT Academy Projects</h1>
+		<header className="relative z-10 flex justify-between items-center bg-gradient-to-r from-purple-600 via-white to-white p-4 shadow-lg rounded-b-lg">
+			<h1 className="text-2xl font-bold text-pink-600">
+				IT Academy Projects
+			</h1>
+
+			<div className="flex space-x-4">
+				<button className="bg-white text-pink-400 shadow rounded-full px-4 py-2 hover:bg-pink-50 transition">
+					Menú 1
+				</button>
+				<button className="bg-white text-pink-400 shadow rounded-full px-4 py-2 hover:bg-pink-50 transition">
+					Menú 2
+				</button>
+			</div>
 		</header>
 	)
 }
